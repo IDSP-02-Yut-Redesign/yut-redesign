d9488651de96c8796e4c872cfc64b04d09f18a50
Devynn <personaldevynn@gmail.com>
2023-05-15 11:43:56 -0700

    add basic scaling

--- public/game.js
+++ public/game.js
@@ -3,6 +3,19 @@ const config = {
   width: 800,
   height: 600,
   scene: [MinigameSelectionScene, TriviaScene, MeteorShowerScene, WordScene, MemoryGameScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      width: 800,
+      height: 600,
+    },
+    max: {
+      width: 1600,
+      height: 900,
+    },
+    zoom: 1,
+  },
   pixelArt: true,
   physics: {
     default: "arcade",
