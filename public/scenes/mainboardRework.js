513e1e7b00a1c0164ebfc11cca1fa1101dfb327f
QubeQode <77304768+QubeQode@users.noreply.github.com>
2023-05-16 19:37:48 -0700

    Add board creation functionality

--- public/scenes/mainboardRework.js
+++ public/scenes/mainboardRework.js
@@ -31,12 +31,13 @@ class GameboardScene extends Phaser.Scene {
     // Symbolic Constants
     #WIDTH;
     #HEIGHT;
-    #SCENE_MANIPULATOR = this.sys;
 
     constructor() {
         super("MainboardScene");
         this.#WIDTH = this.sys.game.config.width;
         this.#HEIGHT = this.sys.game.config.height;
+
+        this.#boardHandler = new BoardHandler (this.#WIDTH, this.#HEIGHT, this.sys);
     };
 
     preload() {
@@ -69,7 +70,13 @@ class GameboardScene extends Phaser.Scene {
     };
 
     create () {
+        // Phaser Groups for Sprite Access
+        this.sys.planets = this.sys.add.group();
+        this.sys.stars = this.sys.add.group();
+        this.sys.markers = this.sys.add.group();
+
         // Board Init Functionality
+        this.#boardHandler.initializeBoard();
     };
 };
 
@@ -127,7 +134,201 @@ class EventHandler {
  * Class handles the rendering of the board and background elements
  */
 class BoardHandler {
+    // Symbolic Constants
+    #HEIGHT;
+    #WIDTH;
+    #BOARD_POSITIONS = {
+        starPositions: [
+            [this.#WIDTH / 2 - 255, this.#HEIGHT / 2 - 75],
+            [this.#WIDTH / 2 - 235, this.#HEIGHT / 2 - 95],
+            [this.#WIDTH / 2 - 165, this.#HEIGHT / 2 - 133],
+            [this.#WIDTH / 2 - 140, this.#HEIGHT / 2 - 142],
+            [this.#WIDTH / 2 - 50, this.#HEIGHT / 2 - 160],
+            [this.#WIDTH / 2 - 22, this.#HEIGHT / 2 - 163],
+            [this.#WIDTH / 2 + 95, this.#HEIGHT / 2 - 157],
+            [this.#WIDTH / 2 + 125, this.#HEIGHT / 2 - 151],
+            [this.#WIDTH / 2 + 240, this.#HEIGHT / 2 - 108],
+            [this.#WIDTH / 2 + 265, this.#HEIGHT / 2 - 90],
+            [this.#WIDTH / 2 + 280, this.#HEIGHT / 2 + 60],
+            [this.#WIDTH / 2 + 255, this.#HEIGHT / 2 + 80],
+            [this.#WIDTH / 2 + 132, this.#HEIGHT / 2 + 135],
+            [this.#WIDTH / 2 + 95, this.#HEIGHT / 2 + 142],
+            [this.#WIDTH / 2 - 60, this.#HEIGHT / 2 + 142],
+            [this.#WIDTH / 2 - 90, this.#HEIGHT / 2 + 137],
+            [this.#WIDTH / 2 - 215, this.#HEIGHT / 2 + 90],
+            [this.#WIDTH / 2 - 240, this.#HEIGHT / 2 + 73],
+        ],
+        planetPositions: {
+            'sun': {
+                position: [this.#WIDTH / 2 - 290, this.#HEIGHT / 2 + 10],
+                scale: 2.25,
+            },
+            'mercury': {
+                position: [this.#WIDTH / 2 - 200, this.#HEIGHT / 2 - 115]
+            },
+            'venus': {
+                position: [this.#WIDTH / 2 - 95, this.#HEIGHT / 2 - 155]
+            },
+            'earth': {
+                position: [this.#WIDTH / 2 + 35, this.#HEIGHT / 2 - 170]
+            },
+            'mars': {
+                position: [this.#WIDTH / 2 + 185, this.#HEIGHT / 2 - 140]
+            },
+            'jupiter': {
+                position: [this.#WIDTH / 2 + 305, this.#HEIGHT / 2 - 25],
+                scale: 1.3
+            },
+            'saturn': {
+                position: [this.#WIDTH / 2 + 200, this.#HEIGHT / 2 + 110]
+            },
+            'uranus': {
+                position: [this.#WIDTH / 2 + 15, this.#HEIGHT / 2 + 145]
+            },
+            'neptune': {
+                position: [this.#WIDTH / 2 - 155, this.#HEIGHT / 2 + 120]
+            },
+        },
+        UIPositions: {
+            centerScreen: [this.#WIDTH/2, this.#HEIGHT/2],
+            fullscreenOption: [this.#WIDTH/1.15, this.#HEIGHT/1.2],
+            playerOne: [this.#WIDTH/2 - 335, this.#HEIGHT/2 - 220],
+            playerTwo: [this.#WIDTH/2 + 335, this.#HEIGHT/2 - 220],
+            playerThree: [this.#WIDTH/2 - 335, this.#HEIGHT/2 + 220],
+            playerFour: [this.#WIDTH/2 + 335, this.#HEIGHT/2 + 220],
+        }
+    };
+    #RENDERER;
 
+    constructor (width, height, renderer) {
+        this.#WIDTH = width;
+        this.#HEIGHT = height;
+        this.#RENDERER = renderer;
+    };
+
+    #createSprite (x, y, spriteName, scaleValue) {
+        return this.#RENDERER.add
+            .sprite(x, y, spriteName)
+            .setScale(scaleValue, scaleValue)
+    };
+
+    #createText (x, y, textContent) {
+        return this.#RENDERER.add
+            .text(x, y, textContent);
+    }
+
+    #renderBackground () {
+        const centerScreen = this.#BOARD_POSITIONS.UIPositions.centerScreen;
+        return this.#createSprite(centerScreen[0], centerScreen[1], 'background', 1);
+    };
+
+    #renderBoardAsset () {
+        const centerScreen = this.#BOARD_POSITIONS.UIPositions.centerScreen
+        return this.#createSprite(centerScreen[0], centerScreen[1], 'finalcompleteboard', 1.5);
+    };
+
+    #renderPlanetNodes () {
+        const allPlanets = Object.keys(this.#BOARD_POSITIONS.planetPositions);
+
+        allPlanets.forEach(planet => {
+            const planetInfo = this.#BOARD_POSITIONS.planetPositions[planet];
+            const planetScale = planetInfo.scale ?? 1.5;
+            const currentPlanet = this.#createSprite(
+                planetInfo.position[0], 
+                planetInfo.position[1], 
+                planet, 
+                planetScale
+            );
+            this.planets.push(currentPlanet);
+        });
+    };
+
+    #renderStarNodes () {
+        this.#BOARD_POSITIONS.starPositions.forEach(position => {
+            const currentStar = this.#createSprite(position[0], position[1], 'star', 1);
+            this.stars.push(currentStar);
+        });
+    };
+
+    #renderPlayerMarkers () {
+        const spriteNames = [
+            'playerone',
+            'playertwo',
+            'playerthree',
+            'playerfour',
+        ];
+
+        spriteNames.forEach(sprite => {
+            const sunPosition = this.#BOARD_POSITIONS.planetPositions.sun.position;
+            const currentMarker = this.#createSprite(
+                sunPosition[0],
+                sunPosition[1],
+                sprite,
+                1
+            );
+            this.markers.push(currentMarker);
+        });
+    };
+
+    #renderOptions () {
+        const fullscreen = this.#createText(this.#WIDTH/1.15, this.#HEIGHT/1.2, 'Fullscreen')
+        fullscreen.setInteractive();
+    };
+
+    #renderPlayerLeaderboard () {
+        const spriteNames = [
+            'playerone',
+            'playertwo',
+            'playerthree',
+            'playerfour',
+        ];
+
+        const playerUIPositions = this.#BOARD_POSITIONS.UIPositions.slice(2);
+
+        for (let i = 0; i < spriteNames.length; i++) {
+            const sprite = this.#createSprite(
+                playerUIPositions[i][0],
+                playerUIPositions[i][1],
+                spriteNames[i],
+                2
+            );
+
+            this.#createText(
+                sprite.x - sprite.width / 2 - 25,
+                sprite.y - sprite.height / 2 + 50,
+                spriteNames[i].replace('r', 'r '),
+                {
+                    fontSize: '18px',
+                    fill: '#ffffff',
+                }
+            );
+
+            this.#createText(
+                sprite.x - sprite.width / 2 - 25,
+                sprite.y - sprite.height / 2 + 70,
+                'Score:',
+                {
+                    fontSize: '18px',
+                    fill: '#ffffff',
+                }
+            );
+        };
+
+    };
+
+    #renderBackgroundUI () {
+        this.#renderOptions();
+        this.#renderPlayerLeaderboard();
+    };
+
+    initializeBoard () {
+        this.#renderBackground();
+        this.#renderBoardAsset();
+        this.#renderPlanetNodes();
+        this.#renderStarNodes();
+        this.#renderPlayerMarkers();
+        this.#renderBackgroundUI();
+    };
 };
 
 /**
